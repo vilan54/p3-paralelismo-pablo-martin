f6122cf41d49f95f1c671a5b0453b9d44aec1c7d
martinlopez03 <113701350+martinlopez03@users.noreply.github.com>
2024-05-02 13:05:21 +0200

    cambios matvec.c

--- matvec.c
+++ matvec.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <mpi.h>
 
@@ -16,7 +17,7 @@ int main(int argc, char *argv[]) {
     float local_vector[N];
     float local_result[N];
     float result[N];
-    struct timeval tv1, tv2;
+    struct timeval tv1, tv2, tv3, tv4;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -31,54 +32,57 @@ int main(int argc, char *argv[]) {
         block_size = padded_block_size;
     }
 
-    // Initialize Matrix and Vector
-    for (i = 0; i < block_size; i++) {
-        for (j = 0; j < N; j++) {
-            matrix[i][j] = (rank * padded_block_size) + i + j;
+    // Initialize Matrix only in process 0
+    if (rank == 0) {
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                matrix[i][j] = i + j;
+            }
+            vector[i] = i;
         }
-        vector[i] = (rank * padded_block_size) + i;
     }
 
-    // Broadcast vector to all processes
-    MPI_Bcast(vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    // Broadcast matrix to all processes
+    MPI_Bcast(matrix, N * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    // Scatter matrix to all processes
-    MPI_Scatter(matrix, N * padded_block_size, MPI_FLOAT, local_matrix, N * padded_block_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    // Distribute vector to all processes
+    MPI_Scatter(vector, N, MPI_FLOAT, vector, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    // Scatter vector to all processes
-    MPI_Scatter(vector, padded_block_size, MPI_FLOAT, local_vector, padded_block_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    gettimeofday(&tv1, NULL); // Time start for computation
 
     // Compute local result
     for (i = 0; i < block_size; i++) {
         local_result[i] = 0;
         for (j = 0; j < N; j++) {
-            local_result[i] += local_matrix[i][j] * local_vector[j];
+            local_result[i] += matrix[i][j] * vector[j];
         }
     }
+
+    gettimeofday(&tv2, NULL); // Time end for computation
 
     // Gather local results
     MPI_Gather(local_result, block_size, MPI_FLOAT, result, block_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
+    gettimeofday(&tv3, NULL); // Time end for communication
+
+    // Print computation time for each process
+    printf("Process %d computation time: %lf seconds\n", rank, (double)(tv2.tv_usec - tv1.tv_usec + (tv2.tv_sec - tv1.tv_sec) * 1e6) / 1e6);
+
+    // Print communication time for each process
+    printf("Process %d communication time: %lf seconds\n", rank, (double)(tv3.tv_usec - tv2.tv_usec + (tv3.tv_sec - tv2.tv_sec) * 1e6) / 1e6);
+
     if (rank == 0) {
-        gettimeofday(&tv1, NULL);
-        // Sum all local results
-        for (i = 1; i < size; i++) {
-            MPI_Recv(result + (i * padded_block_size), padded_block_size, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-        gettimeofday(&tv2, NULL);
+        gettimeofday(&tv4, NULL); // Time end for total execution
 
-        int microseconds = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
+        int microseconds = (tv4.tv_usec - tv1.tv_usec) + 1000000 * (tv4.tv_sec - tv1.tv_sec);
 
-        /*Display result */
         if (DEBUG) {
             for (i = 0; i < N; i++) {
                 printf(" %f \t ", result[i]);
             }
         } else {
-            printf("Time (seconds) = %lf\n", (double)microseconds / 1E6);
+            printf("Total execution time: %lf seconds\n", (double)microseconds / 1E6);
         }
-    } else {
-        MPI_Send(local_result, block_size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
