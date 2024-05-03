d27093be7e2441a57435d746f5a4e591a648a7aa
Pablo Vilan <pablovilan54@gmail.com>
2024-05-03 13:45:53 +0200

    times

--- matvec.c
+++ matvec.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
     float vector[padded_N];
     float local_matrix[block_size][N];
     float local_result[block_size];
-    float t_communication_local, t_computation_local;
+    float t_communication_local, t_computation_local, t_total=0;
     float t_communication[size], t_computation[size];
 
 
@@ -83,9 +83,7 @@ int main(int argc, char *argv[]) {
     MPI_Gather(&t_communication_local, 1, MPI_FLOAT, t_communication, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
     MPI_Gather(&t_computation_local, 1, MPI_FLOAT, t_computation, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
-        int microseconds = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
-        
+    if (rank == 0) {        
         if (DEBUG) {
             for (i = 0; i < N; i++) {
                 printf(" %f \t ", result[i]);
@@ -93,8 +91,10 @@ int main(int argc, char *argv[]) {
         } else {
             printf("\n%2s\t%9s\t%9s\t%9s\n", "Proc", "Comm time (s)", "Comp time (s)", "Total (s)");
             for(int i=0; i<size; i++){
-                printf("%2d\t%2.7f\t%2.7f\t%2.7f\n", i, t_communication[i] , t_computation[i], t_communication[i]+t_computation[i]);
+                printf("%2d\t%2.6lf\t%2.6lf\t%2.6lf\n", i, (double)t_communication[i]/1E6 , (double)t_computation[i]/1E6, (double)(t_communication[i]+t_computation[i])/1E6);
+                t_total += t_communication[i]+t_computation[i];
             }
+            printf("\n\nTotal time: %2.6lf (s)\n", (double)t_total/1E6);
         }
     }
 
